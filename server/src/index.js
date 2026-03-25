c3da93e94f30f8d7502e6aff070a5be9531b0696
MarcoAurelio96 <MarcoAureliolopezcubo@gmail.com>
2026-03-25 13:39:25 +0100

    Añadido enrutamiento centralizado, manejo de excepciones en index.js y mapeo semántico de errores HTTP

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+const { PORT } = require('./config/env');
+const express = require('express');
+const cors = require('cors');
+const taskRoutes = require('./routes/task.routes');
+const errorHandler = require('./middleware/errors')
+
+const app = express();
+
+// Middleware para parsear JSON en el body de las peticiones
+app.use(express.json());
+
+// Middleware para permitir peticiones desde el frontend
+app.use(cors());
+
+// Monta las rutas bajo el prefijo /api/v1/tasks
+app.use('/api/v1/tasks', taskRoutes);
+
+app.use(errorHandler);
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
