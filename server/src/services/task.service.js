492459e5d6efb5ac7e2bc3d1e045faa65475fff9
MarcoAurelio96 <MarcoAureliolopezcubo@gmail.com>
2026-03-25 09:05:22 +0100

    Construida capa de servicios

---
+++ server/src/services/task.service.js
@@ -0,0 +1,40 @@
+let tasks = [];
+
+function obtenerTodas() {
+    return tareas;
+}
+
+function crearTarea(data) {
+    if (!data || !data.title) {
+        throw new Error ('INVALID DATA');
+    }
+
+  const nuevaTarea = {
+    id: Date.now(),
+    title: data.title,
+    completed: false,
+    createdAt: new Date()
+  };
+
+  tareas.push(nuevaTarea);
+
+  return nuevaTarea;
+}
+
+function eliminarTarea(id) {
+  const index = tareas.findIndex(t => t.id === id);
+
+  if (index === -1) {
+    throw new Error('NOT_FOUND');
+  }
+
+  const tareaEliminada = tareas.splice(index, 1);
+
+  return tareaEliminada[0];
+}
+
+module.exports = {
+  obtenerTodas,
+  crearTarea,
+  eliminarTarea
+};
