8d06f51f55b10aa863e5e9a508e060f3fbf465f6
MarcoAurelio96 <MarcoAureliolopezcubo@gmail.com>
2026-03-25 10:36:15 +0100

    implementados los controladores y validación

---
+++ server/src/controllers/task.controller.js
@@ -0,0 +1,50 @@
+const taskService = require('../services/task.service');
+
+const obtenerTodas = (req, res, next) => {
+    try {
+      const tareas = taskService.obtenerTodas();
+      res.status(200).json(tareas);
+    } catch (err) {
+      next (err);
+    }
+};
+
+const crearTarea = (req, res, next) => {
+    try {
+        const { title } = req.body;
+
+        if (!title) {
+            return res.status(400).json({ error: 'El título es obligatorio' });
+        }
+
+        if (typeof tittle !== 'String') {
+            return res.status(400).json({ error: 'El título debe ser texto' })
+        }
+
+        if (title.trim() === ' ') {
+            return res.status(400).json({ error: 'El título no puede estar vacío' })
+        }
+
+        const tarea = taskService.crearTarea({ title });
+        res.status(201).json(tarea);
+      } catch (err) {
+        next(err);
+      }
+};
+
+const eliminarTarea = (req, res, next) => {
+    try {
+        const id = Number(req.params.id);
+
+        if (!Number.isFinite(id)) {
+            return res.status(400).json({ error:'El ID debe ser un número válido' });
+        }
+
+        taskService.eliminarTarea(id);
+        res.status(200).send();
+    }   catch (err) {
+        next (err);
+    }
+};
+
+module.exports = { obtenerTodas, crearTarea, eliminarTarea };
